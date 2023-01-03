82dc36554e1aee6faad4f3fa6a7182c8c053f943
pedrohsa <pedroholanda94@gmail.com>
2023-01-02 21:56:16 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Routes
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, console.log(`Server running at port ${PORT}`));
