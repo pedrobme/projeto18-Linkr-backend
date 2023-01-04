3349106b1ef806b2f8630fc749493a7e688a53db
arthur999999 <arthurhono.contato@gmail.com>
2023-01-04 16:21:41 -0300

    rota /trending 10 # mais postadas

--- src/controllers/hashtags.controllers.js
+++ src/controllers/hashtags.controllers.js
@@ -1,4 +1,14 @@
+import { connectionDB } from "../database/db.js";
 
 export async function trendingHashtags (req, res) {
+
+    try {
+        const trending = await connectionDB.query(`SELECT COUNT("posts-hashtags"."post-id") as "postCount", hashtags.name as "Hashtag" FROM hashtags JOIN "posts-hashtags" ON hashtags.id = "posts-hashtags"."hashtag-id" GROUP BY hashtags.name ORDER BY "postCount" DESC LIMIT 10;`)
+
+        res.send(trending.rows)
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
     
+
 }
