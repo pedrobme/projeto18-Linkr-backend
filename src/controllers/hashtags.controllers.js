be0c7bc21187bfe233eb752005783840cc5bb78e
arthur999999 <arthurhono.contato@gmail.com>
2023-01-04 18:31:45 -0300

    commit new

--- src/controllers/hashtags.controllers.js
+++ src/controllers/hashtags.controllers.js
@@ -1,9 +1,10 @@
-import { connectionDB } from "../database/db.js";
+
+import hashRepository from "../repositories/hashtag.repository.js";
 
 export async function trendingHashtags (req, res) {
 
     try {
-        const trending = await connectionDB.query(`SELECT COUNT("posts-hashtags"."post-id") as "postCount", hashtags.name as "Hashtag" FROM hashtags JOIN "posts-hashtags" ON hashtags.id = "posts-hashtags"."hashtag-id" GROUP BY hashtags.name ORDER BY "postCount" DESC LIMIT 10;`)
+        const trending = await hashRepository.getTrending()
 
         res.send(trending.rows)
     } catch (error) {
@@ -12,3 +13,4 @@ export async function trendingHashtags (req, res) {
     
 
 }
+
