cf87a9a1a2869418a733fdffe326faf667d6d3dc
arthur999999 <arthurhono.contato@gmail.com>
2023-01-05 01:19:33 -0300

    order posts hashtags

--- src/repositories/hashtag.repository.js
+++ src/repositories/hashtag.repository.js
@@ -5,7 +5,7 @@ async function getTrending () {
 }
 
 async function getPostHash(hashtag) {
-    return connectionDB.query(`SELECT posts.id, posts."user-id", posts.date, posts.text, posts.url FROM posts JOIN "posts-hashtags" ON posts.id = "posts-hashtags"."post-id" JOIN hashtags ON "posts-hashtags"."hashtag-id" = hashtags.id WHERE hashtags.name = $1;`, [hashtag])
+    return connectionDB.query(`SELECT posts.id, posts."user-id", posts.date, posts.text, posts.url FROM posts JOIN "posts-hashtags" ON posts.id = "posts-hashtags"."post-id" JOIN hashtags ON "posts-hashtags"."hashtag-id" = hashtags.id WHERE hashtags.name = $1 ORDER BY posts.date DESC;`, [hashtag])
 }
 
 const hashRepository = {
