5a0e8c494f244a2964bbbe458808f2b97c788b69
Lucas Lacerda Buettel <buettellucas@gmail.com>
2023-01-07 20:07:46 -0300

    feat: load post with metadata

--- src/controllers/timeline.controllers.js
+++ src/controllers/timeline.controllers.js
@@ -1,22 +1,56 @@
 import { connectionDB } from "../database/db.js";
+import urlMetadata from "url-metadata";
 
-export async function loadPost (req, res){
 
-    try {        
+
+export async function loadPost(req, res) {
+
+
+    
+
+    try {
+        
         const postsExists = await connectionDB.query(`
         SELECT username, posts.id, image, date, text, url 
         FROM posts 
         JOIN users ON posts."user-id" = users.id 
         ORDER BY date LIMIT 20;
         `)
-        res.send(postsExists.rows);
+
+        const arr = await Promise.all(postsExists.rows.map( async (obj) => {
+            
+
+            let objectNew = {...obj};
+            
+            const metaDatasUrl =  await urlMetadata(obj.url).then(
+                function (metadata) {
+                    /* console.log(metadata.title); */
+                    
+                    objectNew.titleUrl = metadata.title;
+                    objectNew.imageUrl = metadata.image;
+                    objectNew.descriptionUrl = metadata.description;
+
+                },
+                function (error) {
+                    console.log(error)
+                })
+                console.log(objectNew)
+
+            return objectNew;
+
+
+        }))
+        /* console.log(arr) */
+
+        res.send(arr);
 
     }
-    catch(err){
+    catch (err) {
         console.log(err.message);
 
     }
 
 
 }
+
 
