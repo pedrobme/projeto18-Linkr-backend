d46dd8cec8cbae1971399e0421276d42c76669af
arthur999999 <arthurhono.contato@gmail.com>
2023-01-08 13:31:00 -0300

    fix url metadata hashtag route

--- src/controllers/hashtags.controllers.js
+++ src/controllers/hashtags.controllers.js
@@ -1,4 +1,4 @@
-
+import urlMetadata from "url-metadata";
 import hashRepository from "../repositories/hashtag.repository.js";
 
 export async function trendingHashtags (req, res) {
@@ -18,7 +18,31 @@ export async function specificHashtag (req, res){
     const hashtagName = req.params.hashtag
     try {
         const posts = await hashRepository.getPostHash(hashtagName)
-        res.status(200).send(posts.rows)
+
+        const arr = await Promise.all(posts.rows.map( async (obj) => {
+            
+
+            let objectNew = {...obj};
+            
+            const metaDatasUrl =  await urlMetadata(obj.url).then(
+                function (metadata) {
+                    /* console.log(metadata.title); */
+                    
+                    objectNew.titleUrl = metadata.title;
+                    objectNew.imageUrl = metadata.image;
+                    objectNew.descriptionUrl = metadata.description;
+
+                },
+                function (error) {
+                    console.log(error)
+                })
+                console.log(objectNew)
+
+            return objectNew;
+
+        }))
+        
+        res.status(200).send(arr)
     } catch (error) {
         res.status(400).send(error.message)
     }
