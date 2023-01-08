e9e6306ae2c31e1e2c234174a653d889604b477a
pedrohsa <pedroholanda94@gmail.com>
2023-01-07 21:13:32 -0300

    refact: timeline controller fix

--- src/controllers/timeline.controllers.js
+++ src/controllers/timeline.controllers.js
@@ -1,22 +1,15 @@
 import { connectionDB } from "../database/db.js";
 
-export async function loadPost (req, res){
-
-    try {        
-        const postsExists = await connectionDB.query(`
+export async function loadPost(req, res) {
+  try {
+    const postsExists = await connectionDB.query(`
         SELECT username, posts.id, image, date, text, url 
         FROM posts 
         JOIN users ON posts."user-id" = users.id 
-        ORDER BY date LIMIT 20;
-        `)
-        res.send(postsExists.rows);
-
-    }
-    catch(err){
-        console.log(err.message);
-
-    }
-
-
+        ORDER BY date DESC LIMIT 20;
+        `);
+    res.send(postsExists.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
-
