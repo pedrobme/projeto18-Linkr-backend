1b49718a387cc678fba580b6eb776ab415a0d15b
israelpontes <israelpontes@MacBook-Pro-de-israel.local>
2023-01-10 17:23:19 -0300

    feat(redirections)

--- src/controllers/timeline.controllers.js
+++ src/controllers/timeline.controllers.js
@@ -5,9 +5,6 @@ import urlMetadata from "url-metadata";
 
 export async function loadPost(req, res) {
 
-
-    
-
     try {
         
         const postsExists = await connectionDB.query(`
@@ -49,6 +46,51 @@ export async function loadPost(req, res) {
         console.log(err.message);
 
     }
+}
 
 
+export async function otherUserPosts(req, res){
+    const id = req.params.id; 
+   
+    try {
+        const postsExists = await connectionDB.query(`
+        SELECT username, posts.id, image, date, text, url 
+        FROM posts 
+        JOIN users ON posts."user-id" = users.id 
+        WHERE posts."user-id" = $1
+        ORDER BY date DESC LIMIT 20;
+        `, [id])
+
+        const arr = await Promise.all(postsExists.rows.map( async (obj) => {
+            
+            let objectNew = {...obj};
+            
+            const metaDatasUrl =  await urlMetadata(obj.url).then(
+                function (metadata) {
+                    /* console.log(metadata.title); */
+                    
+                    objectNew.titleUrl = metadata.title;
+                    objectNew.imageUrl = metadata.image;
+                    objectNew.descriptionUrl = metadata.description;
+
+                },
+                function (error) {
+                    console.log(error)
+                })
+                console.log(objectNew)
+
+            return objectNew;
+
+
+        }))
+        /* console.log(arr) */
+
+        console.log(id)
+        res.send(arr);
+
+    }
+    catch (err) {
+        console.log(err.message);
+
+    }
 }
--- src/routes/timeline.routes.js
+++ src/routes/timeline.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { loadPost } from "../controllers/timeline.controllers.js";
+import { loadPost, otherUserPosts } from "../controllers/timeline.controllers.js";
 
 const router = Router();
 
 router.get('/timeline', loadPost);
+router.get('/user/:id', otherUserPosts);
 
 
 export default router;
